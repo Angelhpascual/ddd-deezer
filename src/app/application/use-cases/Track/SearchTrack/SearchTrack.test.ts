471a993c9d2222335154700887ce54454a03b105
Angelhpascual <Angelhpascual@gmail.com>
2025-12-12 11:57:11 +0100

    Add SearchTrack use case and update SearchByArtistId test with valid preview URL
    - Introduced SearchTrack use case with a corresponding test to validate track searching functionality.
    - Updated SearchByArtistId test to correct the preview URL format from a malformed to a valid HTTP URL.

--- src/app/application/use-cases/Track/SearchTrack/SearchTrack.test.ts
+++ src/app/application/use-cases/Track/SearchTrack/SearchTrack.test.ts
@@ -1,43 +1,39 @@
 import { ArtistId } from "@/app/domain/Artist/value-objects/ArtistId/ArtistId";
 import { Track } from "@/app/domain/Track/Track";
 import { Duration } from "@/app/domain/Track/value-objects/Duration/Duration";
+import { PreviewUrl } from "@/app/domain/Track/value-objects/PreviewUrl/PreviewUrl";
 import { Rank } from "@/app/domain/Track/value-objects/Rank/Rank";
 import { Title } from "@/app/domain/Track/value-objects/Title/Title";
 import { TrackId } from "@/app/domain/Track/value-objects/TrackId/TrackId";
 import { TrackRepository } from "@/app/domain/TrackRepository";
-import { SearchByArtistId } from "./SearchByArtistId";
 import { describe, expect, it } from "vitest";
-import { PreviewUrl } from "@/app/domain/Track/value-objects/PreviewUrl/PreviewUrl";
-
-
+import { SearchTrack } from "./SearchTrack";
 
 class TrackRepositoryMock implements TrackRepository { 
   async getById(): Promise<Track | null> {
     return null;
   }
- async search(): Promise<Track[]> {
-    return [];
+  async search(): Promise<Track[]> {
+    return [{ 
+      id: TrackId("123"),
+      title: Title("Song Title"),
+      artistId: ArtistId("123"),
+      duration: Duration(120),
+      previewUrl: PreviewUrl("https://www.youtube.com/watch?v=123"),
+      explicit: false,
+      rank: Rank(1),
+    }]
   }
   async searchByArtistId(): Promise<Track[]> {
-    return [
-      {
-        id: TrackId("123"),
-        title: Title("Song Title"),
-        artistId: ArtistId("123"),
-        duration: Duration(120),
-        previewUrl: PreviewUrl("http://example.com/preview.mp3"),
-        explicit: false,
-        rank: Rank(1),
-      }
-    ];
-  }
+    return [];
+  }  
 }
 
-describe("SearchByArtistId", () => {  
-  it("should search for tracks by artist id", async () => {
+describe("SearchTrack", () => {
+  it("should search for tracks", async () => {
     const trackRepository = new TrackRepositoryMock();
-    const searchByArtistId = new SearchByArtistId(trackRepository);
-    const tracks = await searchByArtistId.execute(ArtistId("123"));
+    const searchTrack = new SearchTrack(trackRepository);
+    const tracks = await searchTrack.execute("Song Title");
     expect(tracks).toBeDefined();
     expect(tracks?.length).toBe(1);
   });
